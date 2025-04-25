d962da6e0e4b5d14f7bb3cf578883262a375064d
Youssef Achaab <usfachb@gmail.com>
2025-04-25 11:47:39 +0100

    The image ready for deploy

--- app/movies/views.py
+++ app/movies/views.py
@@ -5,20 +5,30 @@ from rest_framework import status
 
 from .models import Movie
 from .serializers import MovieSerializer
+from django.db import OperationalError
 
 class MoviesList(APIView):
     def post(self, request, format=None):
-        print('View called and request: ', request)
-        serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+        try:
+            serializer = MovieSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except OperationalError:
+                return Response({"error": "Database is not available make sure migrations have been executed."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
     def get(self, request, format=None):
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies, many=True)
-        return Response(serializer.data)
+        try:
+            movies = Movie.objects.all()
+            print('Movies object: ', movies)
+            # what if database is empty or no migrations have been run?
+            if not movies:
+                return Response({"message": "No movies found."}, status=status.HTTP_404_NOT_FOUND)
+            serializer = MovieSerializer(movies, many=True)
+            return Response(serializer.data)
+        except OperationalError:
+            return Response({"error": "Database is not available make sure migrations have been executed."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
 class MovieDetails(APIView):
     def get_object(self, pk):
